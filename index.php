4dee2e0feefbeadabde465723218b59ed470852d
*luis <*luisdalmeida357@gmail.com*>
2020-08-25 23:48:00 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+
+/*todo revisar cálculo de amortização */
+/*fixme corrigir calculo das medias dos alunos */
+
+/**@author luis <luis@provedor.com.br */
+phpinfo();
+
+/**
+ * Class constructor.
+ */
